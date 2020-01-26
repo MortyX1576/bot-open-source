007659fdba96d50732144dc180f93de525504195
Glitch (botdiscord-js) <none>
2020-01-26 21:19:27 +0000

    😇🍡 Checkpoint ./package.json:9502259/3918 ./index.js:9502259/662 ./watch.json:9502259/444

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 const Discord = require('discord.js');
 const fs = require('fs')
+const client = new Discord.Client();
+
+client.on("ready", () => {
+  console.log("BOT ONLINE")
+})
+client.on('raw', async dados => {
+
+})
+client.on("message", async message=> { 
+console.log("opa")
+})
+client.login(process.env.TOKEN)
