dadf61cbc3e23926545c5f9f6e1dc345f8205e3d
Glitch (botdiscord-js) <none>
2020-01-26 21:31:10 +0000

    🌈🍋 Checkpoint ./index.js:9502259/477 ./watch.json:9502259/6 ./package.json:9502259/4580

--- index.js
+++ index.js
@@ -4,11 +4,25 @@ const client = new Discord.Client();
 
 client.on("ready", () => {
   console.log("BOT ONLINE")
+  client.user.setActivity(`Teste Host`, {type: "PLAYING"});
 })
+
 client.on('raw', async dados => {
 
 })
-client.on("message", async message=> { 
-console.log("opa")
+client.on("guildDelete", guild  => {
+
 })
-client.login(process.env.TOKEN)
+client.on("guildCreate", guild => {
+
+})
+client.on('messageReactionAdd', (reaction, user) => {
+
+})
+client.on("guildMemberAdd", async member => {    
+
+})
+client.on("message", async message=> { 
+
+})
+client.login("NjY2MzI4NTY5OTUzMTI0Mzgy.Xi4Cow.SMfAUaiPM1H0h34KU3I_HWb7hbo");
