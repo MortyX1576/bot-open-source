de713b1aeef2dd3cbbbd7fd0caa4e28a66c202a2
Glitch (botdiscord-js) <none>
2020-01-27 17:34:12 +0000

    🚏🏊🏼 Checkpoint ./comandos/config.js:9502259/4188 ./watch.json:9502259/2 ./comandos/ping.js:10698757/2064

--- comandos/config.js
+++ comandos/config.js
@@ -9,9 +9,20 @@ let embed = new Discord.RichEmbed()
 .setTitle("Status Atual")
 .setDescription(`Canal de Registros: ${message.guild.channels.get(dados.canal)}
 Cargo apos Registrar: ${cargo}\n\n
-Para Alterar o Canal de Registro Reaja a '🔧' e Para Alterar o Cargo Reaja a '🛠 '`)
-message.channel.send(embed).then(x =>{
-x.react("🔧 ")
+Para Alterar o Canal de Registro Reaja a '🔧' e Para Alterar o Cargo Reaja a '🛠'`)
+message.channel.send(embed).then(async x =>{
+await x.react("🔧");await x.react("🛠")
+let filtro = (reaction,usuario) => reaction.emoji.name === "🔧"  && usuario.id === message.author.id 
+const coletor = x.creatrReactionCollectior(filtro,{max: 1,time: 30000})
+coletor.on("collect",msg =>{
+message.channel.send("Coloque o Cargo para setar apos o Registro! Use: @")
+let c1 = message.channel.createMessageCollector(x => x.author.id == message.author.id, { time: 60000 * 20,max:1})
+.on('collect', c => {
+cargo = c.mentions.roles.first()
+if(!cargo) return message.channel.send(`❌ | O Cargo ${cargo} Não Existe`)
+dados.cargo
+})
+})  
 })//Mensagem
 }}) //Banco de dados
 }//Evento
