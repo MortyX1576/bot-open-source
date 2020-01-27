e012718f23828ce4b0d40587991d064dc2395435
Glitch (botdiscord-js) <none>
2020-01-27 20:10:12 +0000

    🍚🌻 Checkpoint ./comandos/reload.js:10698757/1265 ./comandos/registro.js:9502259/304 ./watch.json:9502259/8:10698757/6 ./watch.jso}:9502259/2

--- comandos/registro.js
+++ comandos/registro.js
@@ -1,22 +1,20 @@
 const Discord = require("discord.js");
 exports.run = async (client, message, args,Database) => {
   if(!args[0]) return message.channel.send(`❌ | ${message.author} Coloque uma Senha`)  
+  //if(!args.length <= 6) return message.reply("Senha muito Fraca! Use acima de 6 Caracters")
      Database.Usuarios.findOne({ _id: message.author.id}, function(erro, dados) {
-    if (dados) {
-      message.reply("🛑 | Voce já possui uma Conta Criada")      
-    } else {
-      message.channel.send("✅ | ${message.author} Conta Criada com Sucesso!")
+    if(dados) return message.reply("🛑 | Voce já possui uma Conta Criada")    
       message.delete();
+      message.channel.send(`✅ | ${message.author} Conta Criada com Sucesso!`)
       new Database.Usuarios({
+      _id: message.author.id,
       nome: message.author.username,
       tag: message.author.discriminator,
-      senha: args,
+      senha: `${args}`,
       xp: "0",
-      level: "0"
-      .save()
-      })
-    }
-  });
+      level: "0"     
+      }).save()
+     })  
 };
 exports.help = {
   name: "registro"
--- comandos/reload.js
+++ comandos/reload.js
@@ -1,4 +1,5 @@
 const Discord = require ('discord.js')
+const index = require ('../index.js')
 
 exports.run = (bot,message,args) => {
 
@@ -7,9 +8,9 @@ exports.run = (bot,message,args) => {
         bot.comands.delete(args[0])
         const pull = require (`./${args[0]}.js`)
         bot.commands.set(args[0], pull)
+        console.log(pull)
 
     } catch (e) {
-
         return message.channel.send(`<:7685_no:671412271951380500> | Não existe o comando **${args[0].toUpperCase()}**.`); 
     }
     message.channel.send(`<:2990_yes:671412271985065996> | **${args[0].toUpperCase()}** foi reiniciado com sucesso!`);
