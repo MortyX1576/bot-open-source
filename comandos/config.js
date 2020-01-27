47a44d54e6a1c5f3313cab5c9f9a5e1c2bf733d4
Glitch (botdiscord-js) <none>
2020-01-27 18:31:14 +0000

    🔔🏯 Checkpoint ./comandos/config.js:9502259/1008 ./watch.json:9502259/8

--- comandos/config.js
+++ comandos/config.js
@@ -19,30 +19,38 @@ const coletor1 = x.createReactionCollector(num1, {max: 1, time: 60000});
 coletor1.on("collect", msg1 =>{ 
 message.channel.send('⚒ | Coloque o Cargo para Setar! Use: @').then(msg2 => {
 let c2 = message.channel.createMessageCollector(x => x.author.id == message.author.id, { time: 60000 * 20,max:1})
-.on('collect', c => {
+.on('collect',async c => {
 let cargo = c.mentions.roles.first()  
 if(!cargo) return message.channel.send(`🛑 | O Cargo ${cargo} Não Existe`)
 message.channel.send(`✅ | Cargo do Registro alterado para ${cargo}`)
 dados.cargo = cargo.id
 dados.save();
-x.edit(embed)
+setTimeout(() => {
+x.edit(embed);
+x.edit(embed); 
+x.clearReactions();
+msg2.delete();
+}, 2000);  
 })})
 })//coletor
-  
 //coletor 2
 let num2 = (reaction,usuario) => reaction.emoji.name === "🛠" && usuario.id === message.author.id;
 const coletor2 = x.createReactionCollector(num2, {max: 1, time: 60000});
 coletor2.on("collect", msg1 =>{ 
 message.channel.send('⚒ | Coloque o Canal para Setar! Use: #').then(msg2 => {
 let c2 = message.channel.createMessageCollector(x => x.author.id == message.author.id, { time: 60000 * 20,max:1})
-.on('collect', c => {
+.on('collect', async c => {
 let canal = c.mentions.channels.first()  
 if(!canal) return message.channel.send(`🛑 | O Canal ${canal} Não Existe`)
 message.channel.send(`✅ | Canal de Registros alterado para ${canal}`)
 dados.canal = canal.id
 dados.save();
-x.edit(embed)
+setTimeout(() => {
+x.edit(embed);
+x.edit(embed); 
 x.clearReactions();
+msg2.delete();
+}, 2000);  
 })})
 })//coletor
 })//Mensagem
