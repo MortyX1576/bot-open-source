5febebb4062ef999e9908429168b5dd319e3e83a
Glitch (botdiscord-js) <none>
2020-01-27 17:57:54 +0000

    🙏🐡 Checkpoint ./comandos/config.js:9502259/4243 ./watch.json:9502259/4:10698757/2 ./comandos/info.js:10698757/1543

--- comandos/config.js
+++ comandos/config.js
@@ -3,7 +3,8 @@ exports.run = async (client, message, args, Database) =>
 {   
 if(!message.member.hasPermission('ADMINISTRATOR')) return message.reply('❌ | Você não pode usar esse Comando')    
 Database.Guilds.findOne({ _id: message.guild.id}, function(erro, dados) {
-if (dados) {
+if(dados) 
+{
 let cargo = message.guild.roles.get(dados.cargo)
 let embed = new Discord.RichEmbed()
 .setTitle("Configuração do registro:")
@@ -11,9 +12,24 @@ let embed = new Discord.RichEmbed()
 • Cargo após Registrar: ${cargo}\n
 Para alterar o canal de registro reaja '🔧' e para alterar o cargo reaja '🛠'`)
 message.channel.send(embed).then(async x =>{
-await x.react("🔧");await x.react("🛠")//Mensagem
-}}
-  )//Banco de dados
+await x.react("🔧");await x.react("🛠")
+let num1 = (reaction,usuario) => reaction.emoji.name === "🔧" && usuario.id === message.author.id;
+const coletor1 = x.createReactionCollector(num1, {max: 1, time: 60000});
+coletor1.on("collect", msg1 =>{ 
+message.channel.send('⚒ | Coloque o Cargo para Setar! Use: @').then(msg2 => {
+let c2 = message.channel.createMessageCollector(x => x.author.id == message.author.id, { time: 60000 * 20,max:1})
+.on('collect', c => {
+let cargo = c.mentions.roles.first()  
+if(!cargo) return message.channel.send(`🛑 | O Cargo ${cargo} Não Existe`)
+message.channel.send(`✅ | Cargo do Registro alterado para ${cargo}`)
+dados.cargo = cargo.id
+dados.save();
+})})
+})//coletor
+})//Mensagem
+  
+}}) //Banco de dados
+  
 }//Evento
 exports.help = {
     name: 'config'
--- comandos/info.js
+++ comandos/info.js
@@ -4,11 +4,15 @@ exports.run = (bot,message) => {
     const embed = new Discord.RichEmbed()
     .setAuthor("Informações do Bot!")
     .setThumbnail(bot.user.avatarURL)
-    .setDescription(`Olá! Sou o **SMASH**, um bot desenvolvido para **lhe ajudar** no seu servidor!
+    .setDescription(`Olá! Sou o **SMASH**, um bot desenvolvido para **lhe ajudar** no seu servidor!\n
     <:nodejs:671400058708557836> Fui desenvolvido em Java Script!`)
-    .addField('Possuo no momento:', `2 Comandos.`)
-    .addField('Criador:', `\`\`\`cs\n@morty, @miguelws e @godzin!\`\`\``)
+    .addField('Executo:', `2 Comandos`, true)
+    .addField('Meu prefix é:', `!`, true)
+    .addField('Criador:', `\`\`\`cs\n@Morty#4370, @miguelws#9617 e @Godzin#5566\`\`\``)
     .addField('Informações de Guilds', `● Estou presente em [${bot.guilds.size}] Servidores\n● Tenho [${bot.users.size}] Membros ativos!`)
+    .setColor('DARK_AGUA')
+    .setFooter(`Autor: ${message.author.username}`, message.author.avatarURL)
+
     message.channel.send(embed)
 }
 exports.help = {
