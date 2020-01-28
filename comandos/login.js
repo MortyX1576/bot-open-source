e10febb035c4e3e614577ad9ab24cd8cd96f1dbe
Glitch (botdiscord-js) <none>
2020-01-28 11:59:46 +0000

    🚦🎂 Checkpoint ./comandos/teste.js:9502259/1021 ./watch.json:9502259/13 ./comandos/login.js:9502259/304

--- comandos/login.js
+++ comandos/login.js
@@ -7,6 +7,9 @@ let name = message.mentions.members.first() || args[0]
 if(!name) return message.reply("Esse Usuario nao Existe")
 Database.Usuarios.findOne({ _id: name.id || args[0]}, function(erro, dados) {   
 if(dados.logado == true) return message.reply("Voce ja está Logado")
+client.users.forEach(x =>{
+if(x.id == dados.idlogado) return message.reply("Voce Já esta Logado em uma Conta")
+})
 if(dados.senha != args[1]) return message.reply("Senha Incorreta")
 message.channel.send(`Voce Logou ${message.author}`)
 dados.logado = true;
---
+++ comandos/teste.js
@@ -0,0 +1,8 @@
+const Discord = require('discord.js')
+exports.run = async (client, message, args) => 
+{    
+
+}
+exports.help = {
+    name: 'teste'
+}
