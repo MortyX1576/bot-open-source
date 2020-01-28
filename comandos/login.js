1438e020c12abf8ea88ea101ede62baf92e52351
Glitch (botdiscord-js) <none>
2020-01-28 12:23:46 +0000

    🎺🐲 Checkpoint ./comandos/login.js:9502259/3552 ./watch.json:9502259/16

--- comandos/login.js
+++ comandos/login.js
@@ -1,4 +1,5 @@
 const Discord = require('discord.js')
+let Cont =0;
 exports.run = async (client, message, args,Database) => 
 {    
 if(!args[0]) return message.reply("Mencione Seu Nome")
@@ -7,8 +8,20 @@ let name = message.mentions.members.first() || args[0]
 if(!name) return message.reply("Esse Usuario nao Existe")
 Database.Usuarios.findOne({ _id: name.id || args[0]}, function(erro, dados) {   
 //if(dados.logado == true) return message.reply("Voce ja está Logado")
-if(client.users.forEach(x => x.id) == message.author.id) return message.reply("Voce já esta logado em uma Conta")
-  console.log(client.users.forEach(x => x.id))
+client.users.forEach(x =>{
+if(message.author.id == x.id) return message.reply("Voce esta numa conta")
+console.log("Voce logou")
+})
+/*
+if(x.id != message.author.id)
+{
+if(dados.senha != args[1]) return message.reply("Senha Incorreta")
+message.channel.send(`Voce Logou ${message.author}`)
+dados.logado = true;
+dados.idlogado = message.author.id;
+dados.save();
+}else{message.reply("Voce ja está em uma conta")}
+*/
 })
 }
 exports.help = {
