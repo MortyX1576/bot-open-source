071d73f4baa79ed9d7fb14215f5c757a3698fb5e
Glitch (botdiscord-js) <none>
2020-01-29 18:44:36 +0000

    👴🍮 Checkpoint ./comandos/painel.js:10698757/1449 ./watch.json:10698757/2 ./comandos/ping.js:10698757/23

--- comandos/painel.js
+++ comandos/painel.js
@@ -1,17 +1,22 @@
 const Discord = require('discord.js')
+
 exports.run = async (client, message, args,Database) => {
-Database.Usuarios.findOne({ _id: message.author.id}, function(erro, dados) {   //Banco de dados
-message.reply(`Verifique seu privado.`)
-message.author.createDM()
-let embed = new Discord.RichEmbed()
-.setTitle('Informações do membro:')
-.setColor('#a118da')
-.addField(`Seu nome:`, `**${dados.nome}**`, true)
-.addField(`Seu level:`, `**${dados.level}**`, true)
-.addField(`Sua senha:`, `||**${dados.senha}**||`, true)
-.addField(`XP:`, `**${dados.xp}**`, true)
-.addField(`Sua tag:`, `**${dados.tag}**`, true)
+
+    Database.Usuarios.findOne({ _id: message.author.id}, function(erro, dados) {   //Banco de dados
+        message.reply(`Verifique seu privado.`).then (msg => msg.delete(5 * 1000))
+         message.author.createDM()
+
+    let embed = new Discord.RichEmbed()
+        .setTitle('Informações do membro:')
+        .setColor('#a118da')
+        .addField(`• Seu nome:`, `${dados.nome}`, true)
+        .addField(`• Seu level:`, `${dados.level}`, true)
+        .addField(`• Sua senha:`, `||${dados.senha}||`, true)
+        .addField(`• XP:`, `${dados.xp}`, true)
+        .addField(`• Sua tag:`, `${dados.tag}`, true)
+
 message.author.send(embed)
+
 })
 }
 
--- comandos/ping.js
+++ comandos/ping.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js')
-exports.run = async (client, message, args) => 
-{    
+exports.run = async (client, message, args) => {    
+    
     const msg = await message.channel.send(`🏓 Calculando...`);
     let embed = new Discord.RichEmbed()
     .setTitle("Smash Ping:")    
@@ -8,6 +8,7 @@ exports.run = async (client, message, args) =>
     .setDescription(`<:wifi:671435056228728842> Latência: ${Math.floor(msg.createdTimestamp - message.createdTimestamp)}ms!
     <:wifi:671435056228728842> Latência API: ${Math.round(message.client.ping)}ms!`)
     msg.edit(embed) 
+  
 }
 exports.help = {
     name: 'ping'
