3f550f60ea800a7df3586e129c13c6ac9f27046a
Glitch (botdiscord-js) <none>
2020-01-29 17:45:46 +0000

    🏖🐜 Checkpoint ./comandos/ajuda.js:10698757/10148 ./watch.json:10698757/4

--- comandos/ajuda.js
+++ comandos/ajuda.js
@@ -7,27 +7,31 @@ exports.run = async (client, message, args, Database) => {
 
     const embed3 = new Discord.RichEmbed()
     .setTitle("Painel de Ajuda")   
-    .setDescription("▪ Use '⚙️' para ver os comandos da administração.\n▪ Use '🎮' para ver os comandos de utilidade.\n▪ Use '🔙' para voltar.")
+    .setDescription("Olá, sou o **Smash**, um bot **optimizado** e com diversificadas funções, espero que **goste**!")
+    .addField('<:sydneyoperahouse:672134186739564584> | Menu Principal', `Para voltar ao painel principal`)
+    .addField('<:phone:672132596725055488> | Suporte', `Caso queira entrar em contato com os criadores, [clique aqui]()`)
+    .addField('<:seoandweb:672131751031734272> | Menu de comandos', `Veja todos meus comandos`)
     .setColor ("#00FF00")
     .setThumbnail(client.user.avatarURL)
     .setFooter("Página: 1")
     .setTimestamp()
     
         message.author.send(embed3).then(async dados =>{
-        await dados.react("⚙️");
-        await dados.react("🎮");                          
-        let filtro1 = (reaction,usuario) => reaction.emoji.name === "⚙️" && usuario.id === message.author.id
-        let filtro2 = (reaction,usuario) => reaction.emoji.name === "🎮" && usuario.id === message.author.id
+        await dados.react("<:seoandweb:672131751031734272>");
+        await dados.react("<:phone:672132596725055488>");                          
+        let filtro1 = (reaction,usuario) => reaction.emoji.name === "<:seoandweb:672131751031734272>" && usuario.id === message.author.id
+        let filtro2 = (reaction,usuario) => reaction.emoji.name === "<:phone:672132596725055488>" && usuario.id === message.author.id
         let filtro3 = (reaction,usuario) => reaction.emoji.name === "🔙" && usuario.id === message.author.id
         let collector1 = dados.createReactionCollector(filtro1, {max: 10, time: 2*60*1000})
         let collector2 = dados.createReactionCollector(filtro2, {max: 10, time: 2*60*1000})
         let collector3 = dados.createReactionCollector(filtro3, {max: 10, time: 2*60*1000})
+        
         collector1.on('collect', reaction=>{
         if(reaction.emoji.name === "⚙️")
         {
         const embed1 = new Discord.RichEmbed()
-        .setTitle("▸ Administração:")
-        .setDescription("**`▪ <role> <usúario> <cargo>`**\n")
+        .setTitle(" <:seoandweb:672131751031734272> | Menu de comandos")
+        .setDescription("**O meu prefix é:** `!.`\n\n`role` `registro` `warn` ")
         .setColor ("#00FF00")
         .setFooter("Página: 2")
         .setTimestamp()
@@ -35,18 +39,7 @@ exports.run = async (client, message, args, Database) => {
         reaction.remove(message.author.id)
         dados.react("🔙");
         }}) // Coletor 1
-        collector2.on('collect', reaction=>{
-        if(reaction.emoji.name === "🎮")
-        {
-        const embed2 = new Discord.RichEmbed()
-        .setTitle("▸ Utilidade:")
-        .setDescription("**`▪ <perfil> <usúario>`**\n**`▪ <ping>`**")
-        .setColor ("#00FF00")
-        .setFooter("Página: 3")
-        .setTimestamp()
-        dados.edit(embed2)
-        reaction.remove(message.author.id)
-        }}) // Coletor 2
+
         collector3.on('collect', reaction=>{
         if(reaction.emoji.name === "🔙")
         {
