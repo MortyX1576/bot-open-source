2b5f539a9568433375b158150f03e4ff564074b6
Glitch (botdiscord-js) <none>
2020-01-29 18:33:32 +0000

    📯🏬 Checkpoint ./comandos/ajuda.js:10698757/10239 ./watch.json:10698757/6 ./comandos/painel.js:10698757/45

--- comandos/ajuda.js
+++ comandos/ajuda.js
@@ -7,7 +7,7 @@ exports.run = async (client, message, args, Database) => {
 
     const embed3 = new Discord.RichEmbed()
     .setTitle("Painel de Ajuda")   
-    .setDescription("Olá, sou o **Smash**, um bot **optimizado** e com diversificadas funções, espero que **goste**!")
+    .setDescription("Olá, sou o **Smash**. Um bot com várias funções para ajudá-lo na administração do seu servidor!")
     .addField('<:sydneyoperahouse:672134186739564584> | Menu Principal', `Para voltar ao painel principal.`)
     .addField('<:phone:672132596725055488>| Suporte', `Caso queira entrar em contato com os criadores, [clique aqui.](https://discord.gg/Myy9MhY)`)
     .addField('<:seoandweb:672131751031734272> | Menu de comandos', `Veja todos meus comandos.`)
@@ -21,9 +21,9 @@ exports.run = async (client, message, args, Database) => {
         let filtro1 = (reaction,usuario) => reaction.emoji.name === "672131751031734272" && usuario.id === message.author.id
         let filtro2 = (reaction,usuario) => reaction.emoji.name === "672134186739564584" && usuario.id === message.author.id
         let filtro3 = (reaction,usuario) => reaction.emoji.name === "🔙" && usuario.id === message.author.id
-        let collector1 = dados.createReactionCollector(filtro1, {max: 10, time: 2*60*1000})
-        let collector2 = dados.createReactionCollector(filtro2, {max: 10, time: 2*60*1000})
-        let collector3 = dados.createReactionCollector(filtro3, {max: 10, time: 2*60*1000})
+        let collector1 = dados.createReactionCollector(filtro1, {max: 2, time: 2*60*1000})
+        let collector2 = dados.createReactionCollector(filtro2, {max: 2, time: 2*60*1000})
+        let collector3 = dados.createReactionCollector(filtro3, {max: 2, time: 2*60*1000})
         
         collector1.on('collect', reaction=>{
         if(reaction.emoji.name === "672131751031734272")
@@ -35,7 +35,7 @@ exports.run = async (client, message, args, Database) => {
         .setTimestamp()
         dados.edit(embed1)
         reaction.remove(message.author.id)
-        dados.react("672134186739564584");
+        dados.react("672131751031734272");
         }}) // Coletor 1
 
         collector3.on('collect', reaction=>{
