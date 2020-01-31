f62ef89b7fb68da4b1e2de43f38a75428dec71e2
Glitch (botdiscord-js) <none>
2020-01-31 19:26:21 +0000

    🐉📌 Checkpoint ./comandos/s.js:10260136/76:10698757/1290 ./watch.json:10698757/4

--- comandos/s.js
+++ comandos/s.js
@@ -5,12 +5,12 @@ exports.run = (client, message, args) => {
    let channel = message.guild.channels.get('672850878344200228')
 
     const text = args.slice(0).join(' ')
+    if(!text) return message.rep
 
     const embed = new Discord.RichEmbed()
-    .setTitle(`🔔 Sugestão`)
-    .setDescription(` ``${text}``\n\nEnviada por ➝ ${message.author}\n\nDeixe seu feedback aqui em baixo.`)
+    .setTitle(`🔔 | Sugestão`)
+    .setDescription(`${text}\n\nEnviada por ➝ ${message.author}\n\nDeixe seu feedback aqui em baixo.`)
     .setColor('#F0E68C')
-    .setFooter(`Sugestão feito por: ${message.author.tag}!`, message.author.avatarURL)
     
     channel.send(embed).then (msg => msg.react('🎉'))
 
