333148d53f3227ba78d219f6448baea3b412c9d2
Glitch (botdiscord-js) <none>
2020-02-01 16:26:47 +0000

    👂🖇 Checkpoint ./comandos/r.js:10698757/478

--- comandos/r.js
+++ comandos/r.js
@@ -3,11 +3,18 @@ const Discord = require ('discord.js')
 exports.run = (client, message) => {
   
   const embed = new Discord.RichEmbed()
-  .setDescription("Reiniciando o bot.")
-  .setFooter(message.author.tag, message.author.avatarURL)
-  message.channel.send(embed)  
+const Discord = require ('discord.js')
+
+exports.run = (client, message) => {
+
+    const embed = new Discord.RichEmbed()
+    .setDescription("<:7685_no:671412271951380500> | Você não possui ``permissão`` para executar este comando.")
+    .setColor('RED')
+    .setFooter(message.author.tag, message.author.avatarURL)
+
+    message.channel.send()
 }
+
 exports.help = {
   name: 'r'
-
 }
