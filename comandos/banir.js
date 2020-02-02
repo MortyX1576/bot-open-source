5706b067b6ee527afbf09df8c82300b96ee59cef
Glitch (botdiscord-js) <none>
2020-02-02 18:06:00 +0000

    🍕😽 Checkpoint ./comandos/banir.js:10260136/78 ./comandos/money.js:9502259/74:10698757/1623 ./watch.json:10698757/8

--- comandos/banir.js
+++ comandos/banir.js
@@ -6,11 +6,11 @@ const membro = message.mentions.members.first() || message.guild.members.get(arg
 
 if(!message.member.hasPermission("BAN_MEMBERS")) return message.reply("Você não tem permissão para banir membros!") //permissão pra banir
 
-if(membro === message.member) return message.reply("Você não pode banir a si mesmo.") //banir o author da mensagem
+if(membro === message.member) return message.reply("Você não pode banir a si mesmo.") //se banir o author da mensagem
 
-if(!membro) return message.reply("Você não especificou um usuário!") //executar o comando sem o usuário
+if(!membro) return message.reply("Você não especificou um usuário!") //se executar o comando sem o usuário
 
-if(!membro.bannable) return message.reply("Eu não posso banir o usuário, o cargo dele é mais alto que o meu!") //cargo mais alto que o bot
+if(!membro.bannable) return message.reply("Eu não posso banir o usuário, o cargo dele é mais alto que o meu!") //se o cargo for mais alto que o bot
 
 const motivo = args.slice(1).join(' ')
 if(!motivo) return message.reply("Você não especificou um motivo")
@@ -20,7 +20,7 @@ if (motivo) {
 const embed = new Discord.RichEmbed()
 embed.setTitle(":hammer: Banido")
 embed.setColor("RED")
-embed.setDescription(`**•** Usuário banido: **${membro}**\n**•** Banido por: ${message.author.tag}`)
+embed.setDescription(`**•** Usuário banido: **${membro}**\n**•** Banido por: **${message.author.tag}**\n**•** Motivo: ${motivo}`)
 membro.ban()
 message.channel.send(embed)
 }
--- comandos/money.js
+++ comandos/money.js
@@ -1,14 +1,18 @@
 const Discord = require ('discord.js')
 
-exports.run = (client, message, args) => {
+exports.run = (client, message) => {
 
-    const member = message.mentions.members.first() || message.guild.members.get(args[0]);
+    const member = message.mentions.members.first() || message.member;
 
     const embed = new Discord.RichEmbed()
     .setTitle("Smash Money")
-    .setDescription(`O usúario @Godzin possui R$43 reais.`)
-    .setThumbnail("https://cdn.imgbin.com/3/25/21/imgbin-banknote-money-currency-banknote-gold-YdP3QhX3ZbyUSdRXGe8fWqwXv.jpg")
-    .setColor('YELLOW')
+    .addField('O saldo do @migueldeveloper é de:', ` ``R$45 reais.`` `)
+    .setThumbnail("https://img.favpng.com/12/14/9/money-saving-icon-png-favpng-gFFpk3KdkbkyeawSuATTsdSHR.jpg")
+    .setColor('GOLD')
+    .setFooter(message.author.tag, message.author.avatarURL)
 
     message.channel.send(embed)
+}
+exports.help = { 
+    name: 'money'
 }
