fde47ec694e6c774fde2df207672551a8169f406
Glitch (botdiscord-js) <none>
2020-02-02 18:29:50 +0000

    🎤🐏 Checkpoint ./comandos/money.js:10698757/1927 ./watch.json:10698757/6

--- comandos/money.js
+++ comandos/money.js
@@ -1,12 +1,13 @@
 const Discord = require ('discord.js')
 
-exports.run = (client, message) => {
+exports.run = (client, message, args) => {
 
-    const member = message.mentions.members.first() || message.member;
-
+    const member = message.mentions.members.first() || message.guild.members.get(args[0]);
+    if (!member) return message.channel.send()
+  
     const embed = new Discord.RichEmbed()
-    .setTitle("Smash Money")
-    .setDescription("saldo do @migueldeveloper é de: ``R$45 reais.`` ")
+    .setTitle("💵 | Smash Money")
+    .setDescription(`saldo do ${member} é de: R$45 reais. `)
     .setThumbnail("https://img.favpng.com/12/14/9/money-saving-icon-png-favpng-gFFpk3KdkbkyeawSuATTsdSHR.jpg")
     .setColor('GOLD')
     .setFooter(message.author.tag, message.author.avatarURL)
