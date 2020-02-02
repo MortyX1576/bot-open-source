cd625bfcdffc3d7a74a6bf58af064683d970853e
Glitch (botdiscord-js) <none>
2020-02-02 17:08:43 +0000

    🍕🐄 Checkpoint ./comandos/perfil.js:10698757/352 ./watch.json:9502259/2:10698757/2 ./comandos/login.js:9502259/38

--- comandos/login.js
+++ comandos/login.js
@@ -11,8 +11,10 @@ if(!dados) return message.reply("<:7685_no:671412271951380500> | **Conta Inexist
 message.delete();
 if(dados.idlogado > 0) return message.reply("<:7685_no:671412271951380500> | Alguém já está em uma conta.").then (msg => msg.delete(5 * 1000))
 //if(dados.logado == true) return message.reply("Voce ja está Logado")
+  console.log("1")
 if(dados.senha != args[1]) return message.reply("<:7685_no:671412271951380500> | **Senha incorreta.**").then (msg => msg.delete(5 * 1000))
 Database.Usuarios.findOne({idlogado: message.author.id}, function(erro, valor) {     
+  console.log("2")
 if(valor) return message.reply("<:7685_no:671412271951380500> | **Você já está logado.**").then (msg => msg.delete(5 * 1000))
 message.channel.send(`<:2990_yes:671412271985065996> | Você logou na conta: ``${dados.nome}`` `).then (msg => msg.delete(5 * 1000))
 dados.logado = true;
--- comandos/perfil.js
+++ comandos/perfil.js
@@ -2,14 +2,20 @@ const Discord = require ('discord.js')
 const moment = require("moment");
 exports.run = async (client, message, args,Database) => {
    await Database.Usuarios.findOne({idlogado: message.author.id}, function(erro, dados) {   
-   if(!dados) return message.reply("Voce nao está Logado")
+   if(!dados) return message.reply("Você não está Logado!")
+     
+     const embed1 = new Discord.RichEmbed()
+     .setDescription()
+       
+       let on = dados.presence.status.replace("dnd", "Ocupado").replace("idle", "Ausente").replace("offline", "Invisível").replace("online", "Disponível").replace("stream", "Transmitindo")
+     
      const embed = new Discord.RichEmbed()
     .setAuthor(`Informações do ${dados.nome}`)
     .addField('**• Dinheiro:**', dados.dinheiro, true)
     .addField('**• Banco:**', dados.banco, true)
     .addField('**• Xp:**', dados.xp, true)
     .addField('**• Level:**',  dados.level, true)
-    .addField('**• Status:**',  "Online", true)
+    .addField('**• Status:**',  `${on}`, true)
     .setThumbnail(message.member.user.avatarURL)
     .setFooter(dados.tag, message.member.user.avatarURL)
     .setColor('RANDOM')
