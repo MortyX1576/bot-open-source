e4dacfb4285a8d95a595c1ada17dccb958614b6c
Glitch (botdiscord-js) <none>
2020-02-04 17:25:22 +0000

    🎄🌴 Checkpoint ./comandos/daily.js:10698757/1282 ./watch.json:10698757/2

--- comandos/daily.js
+++ comandos/daily.js
@@ -3,6 +3,23 @@ const Discord = require('discord.js')
 exports.run = async (client, message, args,Database) => 
 {    
 
+  
+  
+     /*const embed1 = new Discord.RichEmbed()
+    .setDescription(":no_entry_sign: | Você não está logado.)
+    .setColor('RED')
+    .setFooter(message.author.tag, message.author.avatarURL)
+    .setTimestamp()*/
+    // mensagem de erro
+  
+  
+    const embed = new Discord.RichEmbed()
+    .setDescription(`Você coletou o seu daily! Então recebeu `)
+    .setColor('#eac7ff')
+    .setFooter(message.author.tag, message.author.avatarURL)
+
+    message.channel.send(embed)
+
 }
 exports.help = {
     name: 'daily'
