9abe3af90d526173534795f13759b2b551662f1f
Glitch (botdiscord-js) <none>
2020-02-04 18:00:52 +0000

    🎅🕌 Checkpoint ./comandos/ship.js:10260136/582:10698757/38 ./watch.json:10698757/2

--- comandos/ship.js
+++ comandos/ship.js
@@ -6,11 +6,13 @@ exports.run = (client, message, args) => {
     const member1 = message.mentions.members.first() || message.guild.members.get(args[1])
 
     const fotos = "https://media.giphy.com/media/udiIFmPkJQzkI/giphy.gif"
-    let numeros = ['100%', '84%',]
 
+    let cem = ['100%']
+    let oitenta = ['80%']
+    let numeros = [cem, oitenta]
     const embed = new Discord.RichEmbed()
     .setTitle("Viva o amor ❤️")
-    .setDescription(`O amor é lindo entre o ${member0} e ${member1}! ❤️\nDeu ${numeros} de chances de rolar esse casal!`)
+    .setDescription(`O amor entre ${member0} e {member1} é`+numeros[Math.floor(Math.random() * numeros.length)])
     .setImage(fotos)
 
     message.channel.send(embed)
