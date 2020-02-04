e77714a02f6b9f5799c22757866e770da0395d68
Glitch (botdiscord-js) <none>
2020-02-04 19:19:49 +0000

    🎲🕶 Checkpoint ./package.json:10698757/3483 ./comandos/canvas.js:10698757/178

---
+++ comandos/canvas.js
@@ -0,0 +1,11 @@
+const Discord = require ('discord.js')
+const Canvas = require ('canvas')
+const snekfetch = ('node-superfetch');
+
+exports.run = (client, message, args) => {
+  
+  
+  
+  
+  
+}
