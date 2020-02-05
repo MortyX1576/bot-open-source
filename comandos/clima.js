76e757632388fbc3d9b0700c5f62ddba8aad5282
Glitch (botdiscord-js) <none>
2020-02-05 19:33:50 +0000

    🐂⛑ Checkpoint ./comandos/clima.js:10698757/5392 ./watch.json:10698757/8

--- comandos/clima.js
+++ comandos/clima.js
@@ -6,7 +6,23 @@ exports.run = (client, message, args) => {
   weather.find({search: args.join(" "), degreeType: 'F'}, function(err, result) {
   if(err) message.channel.send(err);
     
-    message.channel.send(JSON.stringify(result[0].current, null, 2));
+    if (result.length=== 0) {
+        message.channel.send("Coloque uma localização correta.").then (msg => msg.delete(5 * 1000))
+        return;
+    }
+
+    const current = result[0].current;
+    const location = result[0].location;
+    
+    const embed = new Discord.RichEmbed()
+    .setAuthor(`Tempo de ${current.observationpoint}`)
+    .addField('Fuso horário:', `${location.timezone}`, true)
+    .addField('Temperatura:', `${current.temperature}`, true)
+    .addField('Umidade:', `${current.humidity}%`, true)
+    .addField('Clima:', `${current.skytext}`, true)
+    .addField('Velocidade do vento:', `${current.windspeed}`, true)
+    .setThumbnail(current.imageUrl)
+    message.channel.send(embed)
 });
   
   
