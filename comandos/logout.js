22b4b3ad60c4fafeaf25060df06849415b8cdb62
Glitch (botdiscord-js) <none>
2020-02-05 17:49:43 +0000

    🐋🍢 Checkpoint ./comandos/logout.js:10698757/1257 ./watch.json:10698757/2

--- comandos/logout.js
+++ comandos/logout.js
@@ -7,11 +7,28 @@ Database.Usuarios.findOne({_id: x.id}, function(erro, result) {
   if(result.idlogado == message.author.id) return Perm = 1;        
 })})
 */
+  //embed
+    const embed1 = new Discord.RichEmbed()
+    .setDescription("Você não está logado.")
+    .setColor('RED')
+    .setFooter(message.author.tag, message.author.avatarURL)
+    .setTimestamp()
+    
+// embed
+    
 await Database.Usuarios.findOne({idlogado: message.author.id}, function(erro, valor) {   
-if(!valor) return message.reply("você não está logado.").then (msg => msg.delete(5 * 1000))
-message.channel.send(`<:2990_yes:671412271985065996> | Você fez logout na conta ${valor.nome}!`).then (msg => msg.delete(5 * 1000))
+if(!valor) return message.channel.send(embed1).then (msg => msg.delete(5 * 1000))
+  
+    const embed = new Discord.RichEmbed()
+    .setDescription(`Você fez logout na conta \`\`\`${valor.nome}\`\`\``)
+    .setColor('RED')
+    .setFooter(message.author.tag, message.author.avatarURL)
+    .setTimestamp()
+      
+message.channel.send(embed).then (msg => msg.delete(5 * 1000))
 Perm = 0;
 valor.idlogado = 0;
+  // `\`\`\`${dados.dinheiro}\`\`\``
 valor.save();
 })
 }
