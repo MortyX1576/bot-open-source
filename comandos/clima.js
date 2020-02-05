a79121af15845e901d0288094695c9d1f80b16f9
Glitch (botdiscord-js) <none>
2020-02-05 20:14:03 +0000

    👽🎲 Checkpoint ./comandos/teste.js:9502259/536 ./watch.json:9502259/4:10698757/4 ./comandos/clima.js:10698757/3740

--- comandos/clima.js
+++ comandos/clima.js
@@ -3,7 +3,7 @@ const weather = require('weather-js');
 
 exports.run = (client, message, args) => {
   
-  weather.find({search: args.join(" "), degreeType: 'F'}, function(err, result) {
+  weather.find({search: args.join(" "), degreeType: 'C'}, function(err, result) {
   if(err) message.channel.send(err);
     
     const current = result[0].current;
@@ -12,7 +12,7 @@ exports.run = (client, message, args) => {
     const embed = new Discord.RichEmbed()
     .setAuthor(`Tempo de ${current.observationpoint}`)
     .addField('Fuso horário:', `${location.timezone}`, true)
-    .addField('Temperatura:',`${current.temperature}`, true)
+    .addField('Temperatura:',`${current.temperature}°C`, true)
     .addField('Tipo de clima:',`${location.degreetype}°`, true)
     .addField('Umidade:', `${current.humidity}%`, true)
     .addField('Clima:', `${current.skytext}`, true)
--- comandos/teste.js
+++ comandos/teste.js
@@ -1,7 +1,11 @@
 const Discord = require('discord.js')
-exports.run = async (client, message, args) => 
+exports.run = async (client, message, args, Database) => 
 {    
+let teste = Database.Usuarios.findOne(function(erro, dados) 
+{   
 
+console.log(dados)
+})
 }
 exports.help = {
     name: 'teste'
