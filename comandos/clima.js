4b57ce099d497308b2a57e5d74fd2256a65432fc
Glitch (botdiscord-js) <none>
2020-02-05 18:58:52 +0000

    👷📠 Checkpoint ./comandos/clima.js:10698757/713 ./watch.json:10698757/2

--- comandos/clima.js
+++ comandos/clima.js
@@ -1,12 +1,12 @@
-const weather = require('weather-js');
 const Discord = require ('discord.js');
+const weather = require('weather-js');
 
 exports.run = (client, message, args) => {
   
   weather.find({search: args.join(" "), degreeType: 'F'}, function(err, result) {
   if(err) message.channel.send(err);
     
-    console.log(JSON.stringify(result, null, 2))
+    message.channel.send(JSON.stringify(result[0].current, null, 2));
 });
   
   
