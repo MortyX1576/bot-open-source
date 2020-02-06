2de6017f6eebca373dc5e46195d9a0b944a79bc7
Glitch (botdiscord-js) <none>
2020-02-06 20:06:50 +0000

    🏨⛺️ Checkpoint ./comandos/moeda.js:10698757/382 ./watch.json:10698757/2

--- comandos/moeda.js
+++ comandos/moeda.js
@@ -1,1 +1,16 @@
-const Discord
+const Discord = require ('discord.js')
+
+exports.run = (client, message, args) => {
+
+    const moeda = args.slice(0).join(" ")
+
+    const embed = new Discord.RichEmbed()
+    .setDescription(`Informações da: ${moeda}:\nhttps://economia.awesomeapi.com.br/all/${moeda}`)
+    .setColor('GOLD')
+
+    message.channel.send(embed)
+
+}
+exports.help = {
+    name: 'moeda'
+}
