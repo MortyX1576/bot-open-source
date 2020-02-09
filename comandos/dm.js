b77df97d4088d43797ca3533e9c6f35b66ec9db3
Glitch (botdiscord-js) <none>
2020-02-09 01:06:43 +0000

    😎😇 Checkpoint ./comandos/dm.js:10698757/4795 ./watch.json:10698757/8

--- comandos/dm.js
+++ comandos/dm.js
@@ -1,11 +1,20 @@
 const Discord = require ('discord.js')
 
 exports.run = (client, message) => {
+
+
+   const embed = new Discord.RichEmbed()
+   .setDescription("Isso foi um teste.")
+   message.author.send(embed).then(x => x.react('😢'))
+  let filtro = (reaction,usuario) => reaction.emoji.name === "📤" && usuario.id === message.author.id;
+  const coletor = x.createReactionCollector(filtro, {max: 1, time: 60000});     
+  coletor.on("collect", msg1 =>{
   
-  message.author.createDM()
-  message.author.send("eae")
+  const embed1 = new Discord.RichEmbed()
+  .setDescription("Eae..")
+    
 }
 
 exports.help = {
-  name: 'dm'
+   name: 'dm'
 }
