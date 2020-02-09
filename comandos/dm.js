8d21f450291d468ac0e5aabe44ebfd7c801b5f13
Glitch (botdiscord-js) <none>
2020-02-09 02:39:32 +0000

    👽🏨 Checkpoint ./comandos/dm.js:10698757/2805 ./watch.json:10698757/8

--- comandos/dm.js
+++ comandos/dm.js
@@ -1,39 +1,31 @@
-const Discord = require ('discord.js')
+const Discord = require('discord.js');
 
 exports.run = async (client, message) => {
   
-        message.author.createDM() // cria uma dm
+  await message.author.createDM(); // cria uma dm
 
-        // embed "principal"
-        const embed = new Discord.RichEmbed()
-        .setDescription("Exemplo.")
-    
-        message.author.send(embed).then(async dados =>{
-        await dados.react("⚙️");    
-        await dados.react("🙂");                        
-        let filtro1 = (reaction,usuario) => reaction.emoji.name === "⚙️" && usuario.id === message.author.id
-        let filtro2 = (reaction,usuario) => reaction.emoji.name === "🙂" && usuario.id === message.author.id
-        let collector1 = dados.createReactionCollector(filtro1, {max: 1, time: 2*60*1000})
-        let collector2 = dados.createReactionCollector(filtro2, {max: 1, time: 2*60*1000})
+  // embed "principal"
+  const embed = new Discord.RichEmbed().setDescription('Exemplo.');
 
-        // se o usúario reagiu ao emoji, ele vai coletar
-        collector1.on('collect', reaction=>{
-        if(reaction.emoji.name === "⚙️") // 
-        {
-        const embed1 = new Discord.RichEmbed()
-        .setDescription("Exemplo 1.")
-        dados.edit(embed1) // ai aqui 'ele' vai editar a embed principal e vai substituir por essa..
-        }
+  message.author.send(embed).then(async dados => {
+    await dados.react('⚙️');
+    await dados.react('🙂');
+    let filtro = (reaction, usuario) => (reaction.emoji.name === '⚙️' || reaction.emoji.name === '🙂') && usuario.id === message.author;
+    let collector = dados.createReactionCollector(filtro, { max: 1, time: 2 * 60 * 1000 });
 
-        collector2.on('collect', reaction=>{
-         if(reaction.emoji.name === "🙂") 
-         {
-         const embed2 = new Discord.RichEmbed()
-         .setDescription("Exemplo 1.")
-         dados.edit(embed2) 
-         }
-   })})
-})}
+    // se o usúario reagiu ao emoji, ele vai coletar
+    collector.on('collect', reaction => {
+      if (reaction.emoji.name === '⚙️') {
+        //
+        const embed1 = new Discord.RichEmbed().setDescription('Exemplo 1.');
+        dados.edit(embed1); // ai aqui 'ele' vai editar a embed principal e vai substituir por essa..
+      } else if (reaction.emoji.name === '🙂') {
+        const embed2 = new Discord.RichEmbed().setDescription('Exemplo 2.');
+        dados.edit(embed2);
+      }
+    });
+  });
+};
 
 exports.help = {
   name: 'dm'
