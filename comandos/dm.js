282f4e8216c171556d50d35aebafb829e9704248
Glitch (botdiscord-js) <none>
2020-02-09 01:40:29 +0000

    🍪🐧 Checkpoint ./comandos/dm.js:10698757/16027 ./watch.json:10698757/14

--- comandos/dm.js
+++ comandos/dm.js
@@ -2,32 +2,31 @@ const Discord = require ('discord.js')
 
 exports.run = async (client, message,args) => {
 
-     await message.author.createDM()
-     message.channel.send(`${message.author}, olhe sua caixa de mensagens!`)
+     message.channel.send(`${message.author}, olhe sua caixa de mensagens!`).then (msg => msg.delete(5000))
 
     const embed3 = new Discord.RichEmbed()
-    .setTitle("Teste")   
-    .setDescription("David é lindo!")
+    .setTitle("Painel de Ajuda")   
+    .setDescription("**▪ Use '⚙️' para ver os comandos da administração.\n▪ Use '🎮' para ver os comandos de utilidade.\n▪ Use '🔙' para voltar.**")
+    .setColor ("#00FF00")
+    .setFooter("Página: 1")
     .setTimestamp()
     
         message.channel.send(embed3).then(async dados =>{
-        await dados.react("🎮");
-                         
-        let filtro1 = (reaction,usuario) => reaction.emoji.name === "🎮" && usuario.id === message.author.id
-
+        await dados.react("⚙️");
+                                 
+        let filtro1 = (reaction,usuario) => reaction.emoji.name === "⚙️" && usuario.id === message.author.id
         let collector1 = dados.createReactionCollector(filtro1, {max: 1, time: 2*60*1000})
 
         collector1.on('collect', reaction=>{
         if(reaction.emoji.name === "⚙️")
         {
         const embed1 = new Discord.RichEmbed()
-        .setTitle("Teste.")
-        .setDescription("Eae mano, miguelws é lindão!")
+        .setDescription("Teste.")
+        .setColor ("#00FF00")
         dados.edit(embed1)
         // <a:RingingBell:671788063063670824>
 // a:loading:671469442957574214>
-        }}) // Coletor 1
-       
+        }}) // Co
 })
 }
 
