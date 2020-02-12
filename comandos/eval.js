cbc5ad0d27302775626ad23b123ed05b0139ee16
Glitch (botdiscord-js) <none>
2020-02-12 19:38:46 +0000

    🏘🗽 Checkpoint ./watch.json:9502259/14 ./comandos/eval.js:9502259/366

--- comandos/eval.js
+++ comandos/eval.js
@@ -6,11 +6,9 @@ exports.run = async (bot, message, args) =>
     let evaled;
     try {
       evaled = await eval(args.join(' '));
-      const embed = new Discord.RichEmbed()
-      .setDescription(`
-      Entrada: ${args}
-      Saida: ${inspect(evaled)} 
-      `)
+      const embed = new Discord.RichEmbed()        
+      .addField(`Entrada:`,` ${args}`)      
+      .addField(`Saida:`,` ${inspect(evaled)}`);       
       message.channel.send(embed);
     }
     catch (error) {
