3137f6875a5c2bddd5742f01c1d3e8480a20dc57
Glitch (botdiscord-js) <none>
2020-02-13 19:50:54 +0000

    🍀🐈 Checkpoint ./embed.json:9502259/46 ./watch.json:9502259/6:10698757/10 ./comandos/teste.js:9502259/46 ./comandos/help.js:10698757/2053

---
+++ comandos/help.js
@@ -0,0 +1,18 @@
+const Discord = require ('discord.js')
+
+exports.run = (client, message) => {
+
+    const embed = new Discord.RichEmbed().setAuthor(message.author.tag, message.author.avatarURL)
+    .setDescription('Aqui está o meu menu de comandos, espero que goste!')
+    .addField('🚔 Moderadores', "``banir`` ``limpar`` ``config``")
+    .addField('🔑 Utilidades:', "``clima`` ``avatar`` ``calcular`` ``info`` ``login`` ``logout`` ``nivel``\n``painel`` ``perfil`` ``prefixo`` ``registro``")
+    .addField('💸 Economia', "``saldo`` ``daily`` ``addgrana``")
+    .addField('🖥️ Desenvolvedores:', "``eval`` ``botinfo``")
+    .setColor('#00FFFF')
+
+    message.channel.send(embed)
+}
+
+exports.help = {
+    name:'help'
+}
--- comandos/teste.js
+++ comandos/teste.js
@@ -1,7 +1,8 @@
 const Discord = require('discord.js')
-const teste = require('../embed.js')
+const teste = require('../embed.json')
 exports.run = async (client, message, args) => {    
   console.log(teste)
+  message.channel.send(teste.embed)
   
 }
 exports.help = {
