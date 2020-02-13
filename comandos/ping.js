5f88336df1aede85b5d12042362e6239f4a5c9ed
Glitch (botdiscord-js) <none>
2020-02-13 21:37:53 +0000

    🌻💺 Checkpoint ./comandos/ping.js:9502259/3

--- comandos/ping.js
+++ comandos/ping.js
@@ -8,7 +8,6 @@ exports.run = async (client, message, args) => {
     .setDescription(`<:wifi:671435056228728842> Latência: ${Math.floor(msg.createdTimestamp - message.createdTimestamp)}ms!
     <:wifi:671435056228728842> Latência API: ${Math.round(message.client.ping)}ms!`)
     msg.edit(embed) 
-  
 }
 exports.help = {
     name: 'ping'
