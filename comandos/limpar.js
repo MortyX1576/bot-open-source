6fc770da3d849722cc316e05a48b7d57b042d8f4
Glitch (botdiscord-js) <none>
2020-02-14 20:49:36 +0000

    🚟📒 Checkpoint ./comandos/limpar.js:11420172/705 ./embed.json:11420172/51 ./watch.json:11420172/6

--- comandos/limpar.js
+++ comandos/limpar.js
@@ -9,15 +9,19 @@ exports.run = async (client,message,args) => {
     .setFooter(message.author.tag, message.author.avatarURL)
     .setTimestamp()
     
-let embed2 = new Discord.RichEmbed()
-.setDescription(`${message.author}, ${teste.number}`)
-.setColor("RANDOM")
+    let embed2 = new Discord.RichEmbed()
+    .setDescription(`${message.author}, ${teste.number}`)
+    .setColor("RANDOM")
+    
+    let embed3 = new Discord.RichEmbed()
+    .setDescription(`${message.author}, ${teste.helpc}`)
+    .setColor("RANDOM")
 
 if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send(embed1).then (msg => msg.delete(5 * 1000))
 
-if(!args[0]) return message.reply("Use !limpar (quantidade).").then (msg => msg.delete(3 * 1000))
+if(!args[0]) return message.channel.send(embed3).then (msg => msg.delete(8 * 1000))
 let numero = args[0];
-if(isNaN(numero)) return message.channel.send(embed2).then (msg => msg.delete(3 * 1000))
+if(isNaN(numero)) return message.channel.send(embed2).then (msg => msg.delete(8 * 1000))
 numero = parseInt(numero)
 if(numero >= 100 || numero <= 1) return message.reply("Coloque um valor abaixo de 100 e maior que 1.").then (msg => msg.delete(3 * 1000))
 message.channel.bulkDelete(numero + 1).then(() => {
