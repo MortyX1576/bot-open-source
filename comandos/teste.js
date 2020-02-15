f3a8039aa1ec9017f5cfd2b128ad9efb839770e3
Glitch (botdiscord-js) <none>
2020-02-15 12:16:10 +0000

    🕍🎗 Checkpoint ./comandos/teste.js:9502259/156 ./watch.json:9502259/2

--- comandos/teste.js
+++ comandos/teste.js
@@ -2,11 +2,7 @@ const Discord = require('discord.js')
 const teste = require('../embed.json')
 
 exports.run = async (client, message, args, Database) => {    
-    Database.Guilds.findOne({ _id: message.guild.id}, function(erro, dados) {  
-    let x =   
-    dados.teste = x;
-    dados.save();
-    })
+Database.Guilds.findOne({ _id: message.guild.id},{id: [ "1", "2", "3"]})
 }
 exports.help = {
     name: 'teste'
