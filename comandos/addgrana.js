f0352a632fcccd65fcfa02c6703bd01c8d54c71b
Glitch (botdiscord-js) <none>
2020-02-17 21:03:31 +0000

    🍞🌠 Checkpoint ./comandos/addgrana.js:11420172/101 ./watch.json:11420172/2

--- comandos/addgrana.js
+++ comandos/addgrana.js
@@ -3,13 +3,13 @@ const Discord = require ('discord.js')
 exports.run = (client, message, args,Database) => {
   
   const member = message.mentions.members.first() || message.guild.members.get(args[0])
-  if(!member) return message.reply("Usuario Invalido")
+  if(!member) return message.reply("usuário inválido!")
   Database.Usuarios.findOne({ _id: member.id}, function(erro, valor) {    
   if(!valor) return message.reply("Usuário não cadastrado!")
   if(isNaN(args[1])) return message.reply("Coloque um valor em número!")
     
   const embed = new Discord.RichEmbed()
-  .setDescription(`✅ | Adicionado \`\`jsR$${args[1]}\`\` reais na conta do ${member}!`)
+  .setDescription(`<:money:679059862591635475> Adicionado \`\`R$${args[1]}\`\` reais na conta do ${member}!`)
   .setColor('#00FF7F')  
   message.channel.send(embed).then (msg => msg.delete(6 * 1000)) 
   valor.dinheiro += args[1];
