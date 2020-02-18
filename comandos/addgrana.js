52aa646a7ae39f0204eba9d5e6736cd566c6807b
Glitch (botdiscord-js) <none>
2020-02-18 17:35:48 +0000

    🌈🎊 Checkpoint ./comandos/addgrana.js:9502259/194:11420172/154 ./watch.json:9502259/14:11420172/2 ./comandos/eval.js:9502259/96

--- comandos/addgrana.js
+++ comandos/addgrana.js
@@ -6,16 +6,19 @@ exports.run = (client, message, args,Database) => {
   if(!member) return message.reply("usuário inválido!")
   Database.Usuarios.findOne({ _id: member.id}, function(erro, valor) {    
   if(!valor) return message.reply("Usuário não cadastrado!")
-  if(isNaN(args[1])) return message.reply("Coloque um valor em número!")
   if(args[1].includes('.' || ',')) return message.reply("você não pode usar números com vírgulas ou ponto.")
+  if(isNaN(args[1])) return message.reply("Coloque um valor em número!")
     
   const embed = new Discord.RichEmbed()
   .setDescription(`<:money:679059862591635475> Adicionado \`\`R$${args[1]}\`\` reais na conta do ${member}!`)
   .setColor('#00FF7F')  
   message.channel.send(embed).then (msg => msg.delete(6 * 1000)) 
   let value = valor.dinheiro + args[1];
-  console.log(value)
-  message.channel.send(`Banco de Dados: ${valor.dinheiro} Argumentos: ${args[1]} Soma: ${valor.dinheiro += args[1]}`)
+  let x = valor.dinheiro;
+  let y = args[1]
+  let z = x + y;
+  console.log(z)
+  message.channel.send(`${z}`)
   valor.dinheiro = 0;
   valor.save();
   })
