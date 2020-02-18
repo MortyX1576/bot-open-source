be88f8b9639b5377a2bc3039e7b5aed313939bfd
Glitch (botdiscord-js) <none>
2020-02-18 18:09:44 +0000

    🍎🛫 Checkpoint ./comandos/info.js:11420172/643

--- comandos/info.js
+++ comandos/info.js
@@ -1,13 +1,13 @@
 const Discord = require ('discord.js')
 
 exports.run = (bot,message) => {
+  
     const embed = new Discord.RichEmbed()
-    .setAuthor("Informações do Bot!")
-
-    .setDescription(`Olá! Sou o **SMASH**, um bot desenvolvido para **lhe ajudar** no seu servidor!`)
-    .addField('<:nodejs:671400058708557836> Desenvolvido em:', `Java Script!`, true)
-    .addField('<:unnamed:671417880847581323> Total de comandos:', `2 Comandos`, true)
-    .addField('<:01512:671417880847581222> Meu prefix é:', `!`, true)
+    .setAuthor("Informações do Bot..")
+    .setDescription("```Bom, eu fui criado para ajudá-lo na administração e moderação do seu servidor, tenho várias funções, espero que goste e tenha um bom entretenimento!```")
+    .addField('Desenvolvido em:', `<:javascript:679388315916697610> Java Script!`, true)
+    .addField('📁 Total de comandos:', `Ainda vou terminar ;-;`, true)
+    .addField(' Meu prefix é:', `!`, true)
     .addField('Informações de Guilds', `● Estou presente em [${bot.guilds.size}] Servidores\n● Tenho [${bot.users.size}] Membros ativos!`)
     .addField('Criadores:', `\`\`\`\nMorty#4370, miguelws#9617 e Godzin#5566.\`\`\``)
     .setColor('DARK_GREY')
